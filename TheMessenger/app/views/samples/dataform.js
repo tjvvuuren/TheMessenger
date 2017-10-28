cf84a6d54f564049bddee8beb78be837676e181f
Theunis Janse van Vuuren <theunis@leapfrog.global>
2017-10-28 15:34:58 +0000

    Changes made by Theunis Janse van Vuuren (theunis@leapfrog.global).

--- TheMessenger/app/views/samples/dataform.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var viewModel = require("./person-model");
-function onPageLoaded(args) {
-    var page = args.object;
-    page.bindingContext = new viewModel.PersonViewModel();
-}
-exports.onPageLoaded = onPageLoaded;
-//# sourceMappingURL=dataform.js.map
--- TheMessenger/app/views/samples/dataform.ts
+++
@@ -1,5 +0,0 @@
-import viewModel = require("./person-model");
-export function onPageLoaded(args) {
-    var page = args.object;
-    page.bindingContext = new viewModel.PersonViewModel();
-}
